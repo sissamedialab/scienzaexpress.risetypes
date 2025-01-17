64f81e97e6a67ba13e06a2cadb8dd76e25b4adba
Matteo Gamboz <gamboz@medialab.sissa.it>
2025-01-17 18:23:49 +0100

    [exp] add view host an action
    - plonecli add view

---
+++ src/scienzaexpress/risebook/tests/test_view_create_pages.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from scienzaexpress.risebook.testing import SCIENZAEXPRESS_RISEBOOK_FUNCTIONAL_TESTING
+from scienzaexpress.risebook.testing import SCIENZAEXPRESS_RISEBOOK_INTEGRATION_TESTING
+from scienzaexpress.risebook.views.create_pages import ICreatePages
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.interface.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = SCIENZAEXPRESS_RISEBOOK_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_create_pages_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='create-pages'
+        )
+        self.assertTrue(ICreatePages.providedBy(view))
+
+    def test_create_pages_not_matching_interface(self):
+        view_found = True
+        try:
+            view = getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='create-pages'
+            )
+        except ComponentLookupError:
+            view_found = False
+        else:
+            view_found = ICreatePages.providedBy(view)
+        self.assertFalse(view_found)
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = SCIENZAEXPRESS_RISEBOOK_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
---
+++ src/scienzaexpress/risebook/views/create_pages.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# from scienzaexpress.risebook import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class ICreatePages(Interface):
+    """ Marker Interface for ICreatePages"""
+
+
+@implementer(ICreatePages)
+class CreatePages(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('create_pages.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
