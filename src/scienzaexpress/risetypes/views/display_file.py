b0d11c74f675fc42bf81ec7263fa369e011dcaa0
Matteo Gamboz <gamboz@medialab.sissa.it>
2026-01-19 14:24:03 +0100

    force "inline"

--- src/scienzaexpress/risetypes/views/display_file.py
+++ src/scienzaexpress/risetypes/views/display_file.py
@@ -5,17 +5,7 @@ from zope.publisher.interfaces import NotFound
 class DisplayFile(Download):
     """Serve file inline (for PDF viewing in browser)."""
 
-    def __call__(self):
-        file = self._getFile()
-        if file is None:
-            raise NotFound(self, self.fieldname, self.request)
-        import pdb
-
-        pdb.set_trace()
-        self.set_headers(file)
-        # Override Content-Disposition to inline instead of attachment
-        filename = getattr(file, "filename", self.fieldname)
-        self.request.response.setHeader(
-            "Content-Disposition", f'inline; filename="{filename}XXX"'
-        )
-        return super().__call__()
+    def set_headers(self, file) -> None:
+        """Force content disposition to "inline"."""
+        super().set_headers(file)
+        self.request.response.setHeader("Content-Disposition", "inline")
