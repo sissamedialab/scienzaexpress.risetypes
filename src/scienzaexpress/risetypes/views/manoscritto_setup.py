a7191b6152bde40445cca214f625b9dbbb8dcb25
Matteo Gamboz <gamboz@medialab.sissa.it>
2026-01-26 12:19:48 +0100

    edit ManoscrittoSetup's __call__() method
    make it similar to AlboSetup's __call__() method;
    
    ManoscrittoSetup.__call__() should create the following FolderNodes:
    - Prima valutazione
    - Lettura redazionale
    - Decisione editoriale
    - Per l'istruttoria

--- src/scienzaexpress/risetypes/views/manoscritto_setup.py
+++ src/scienzaexpress/risetypes/views/manoscritto_setup.py
@@ -1,18 +1,47 @@
 # -*- coding: utf-8 -*-
 
+from ..lib import FolderNode
+from plone import api
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
 
 
 class IManoscrittoSetup(Interface):
-    """Marker Interface for IManoscrittoSetup"""
+    """Marker Interface for IManoscrittoSetup."""
 
 
 @implementer(IManoscrittoSetup)
 class ManoscrittoSetup(BrowserView):
     def __call__(self):
-        template = """<li class="heading" i18n:translate="">
-          Sample View
-        </li>"""
-        return template
+        """Set up Manoscritto."""
+        folders = [
+            FolderNode("Prima valutazione"),
+            FolderNode("Lettura redazionale"),
+            FolderNode("Decisione editoriale"),
+            FolderNode("Per l'istruttoria"),
+        ]
+
+        # We don't have a "root" but must work on a list
+        created = []
+        existing = []
+        for folder in folders:
+            sub_created, sub_existing = folder.create(self.context)
+            created.extend(sub_created)
+            existing.extend(sub_existing)
+
+        # Report a warning for each folder that already existed.
+        # No need to say anything about created ones.
+        for folder in existing:
+            api.portal.show_message(
+                message=f'la cartella "{folder}" esiste già.',
+                request=self.request,
+                type="warning",
+            )
+
+        api.portal.show_message(
+            message="Manoscritto setup completed.",
+            request=self.request,
+            type="success",
+        )
+        return self.request.response.redirect(self.context.absolute_url())
